6f619dd71c81bc23e5a3dea4a1d8ae117087cd87
tushar-coditas <tushar.baweja@coditas.com>
2016-06-06 19:24:14 +0530

    Update MainActivity.java

--- android-camera-app/src/com/hmkcode/android/MainActivity.java
+++ android-camera-app/src/com/hmkcode/android/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		intent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath); */
 		
 		// start camera activity
-	    startActivityForResult(intent, TAKE_PICTURE);
+	    //startActivityForResult(intent, TAKE_PICTURE);
 
 	}
 	
